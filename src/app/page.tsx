324566edd0c36e0e40a27c6c4c97fb9b59b46b29
Daniel Silva <danielzcomputacao@gmail.com>
2023-07-23 13:34:37 -0300

    putting the state in ProductsContainer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,8 @@
 
 import { ProductsContainer } from "../components/ProductsContainer";
 import "../css/global.css";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        fetch("https://dummyjson.com/products")
-            .then((res) => res.json())
-            .then(console.log);
-    }, []);
-
     return (
         <div className="w-full flex items-center pt-20 flex-col min-h-screen max-h-max bg-slate-400">
             <ProductsContainer />
--- src/components/ProductsContainer.tsx
+++ src/components/ProductsContainer.tsx
@@ -1,7 +1,15 @@
+import { useEffect, useState } from "react";
 import { Input } from "./Products/Input";
 import { Product } from "./Products/Product";
 
+useEffect(() => {
+    fetch("https://dummyjson.com/products")
+        .then((res) => res.json())
+        .then(console.log);
+}, []);
+
 export const ProductsContainer = () => {
+    const [products, setProducts] = useState([]);
     return (
         <>
             <Input />
