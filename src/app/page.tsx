ca01ef6e481ab8d1662694cad62553105a3063b4
Daniel Silva <danielzcomputacao@gmail.com>
2023-07-23 13:30:42 -0300

    adding productContainer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { Product } from "@/components/Product";
-import { Input } from "../components/Input";
+import { ProductsContainer } from "../components/ProductsContainer";
 import "../css/global.css";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("https://dummyjson.com/products")
+            .then((res) => res.json())
+            .then(console.log);
+    }, []);
+
     return (
         <div className="w-full flex items-center pt-20 flex-col min-h-screen max-h-max bg-slate-400">
-            <Input />
-            <br />
-            <Product></Product>
+            <ProductsContainer />
         </div>
     );
 }
---
+++ src/components/ProductsContainer.tsx
@@ -0,0 +1,12 @@
+import { Input } from "./Products/Input";
+import { Product } from "./Products/Product";
+
+export const ProductsContainer = () => {
+    return (
+        <>
+            <Input />
+            <br />
+            <Product></Product>
+        </>
+    );
+};
