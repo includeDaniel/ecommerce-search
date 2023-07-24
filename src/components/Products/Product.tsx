59ef4166f7307460a601cf4490b147e466df263f
Daniel Silva <danielzcomputacao@gmail.com>
2023-07-23 22:50:32 -0300

    type ProductProps created

--- src/components/Products/Product.tsx
+++ src/components/Products/Product.tsx
@@ -1,44 +1,43 @@
-const product = {
-    id: 29,
-    title: "Handcraft Chinese style",
-    description:
-        "Handcraft Chinese style art luxury palace hotel villa mansion home decor ceramic vase with brass fruit plate",
-    price: 60,
-    discountPercentage: 15.34,
-    stock: 7, //in stock (7) items
-    images: [
-        "https://i.dummyjson.com/data/products/29/1.jpg",
-        "https://i.dummyjson.com/data/products/29/2.jpg",
-        "https://i.dummyjson.com/data/products/29/3.webp",
-        "https://i.dummyjson.com/data/products/29/4.webp",
-        "https://i.dummyjson.com/data/products/29/thumbnail.webp",
-    ],
+type ProductProps = {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    stock: number;
+    image: string;
 };
-export const Product = () => {
+
+export const Product = ({
+    id,
+    title,
+    description,
+    price,
+    discountPercentage,
+    stock,
+    image,
+}: ProductProps) => {
     return (
         <div className="w-72 h-min bg-white p-5">
             <img
                 className="h-44 w-56 border-black"
-                src={product.images[0]}
+                src={image}
                 alt="product image"
             />
-            <span className="text-base">{product.title}</span>
+            <span className="text-base">{title}</span>
             <hr />
-            <span className="text-xs text-gray-500">{product.description}</span>
+            <span className="text-xs text-gray-500">{description}</span>
             <div className="flex place-content-start pt-5 text-lg items-center">
                 <span className="text-slate-950 line-through pr-3">
-                    {product.price}R$
+                    {price}R$
                 </span>
 
                 <span className="text-red-700">
-                    {(
-                        product.price -
-                        product.price * (product.discountPercentage / 100)
-                    ).toFixed(2)}
+                    {(price - price * (discountPercentage / 100)).toFixed(2)}
                     R$
                 </span>
                 <span className="pl-10 text-xs text-gray-700">
-                    {product.stock} in stock
+                    {stock} in stock
                 </span>
             </div>
         </div>
