ea614edf8ed65f1a551e23361ff3b946718886b3
Daniel Silva <danielzcomputacao@gmail.com>
2023-08-02 15:27:08 -0300

    screens width adjusted to start the responsivity

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
+        screens: {
+            cellphone: "320px",
+            // => @media (min-width: 320px) { ... }
+
+            tablet: "560px",
+            // => @media (max-width: 560px) { ... }
+
+            laptop: "800px",
+            // => @media (min-width: 800px) { ... }
+
+            desktop: "1280px",
+            // => @media (min-width: 1280px) { ... }
+        },
         extend: {
             backgroundImage: {
                 "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
