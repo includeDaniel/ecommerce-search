c7c4eb606f8b297c7b02041cdc186aeb47bcbd3b
Daniel Silva <danielzcomputacao@gmail.com>
2023-08-21 17:50:33 -0300

    adding error status

--- src/components/ProductSearch/Products.tsx
+++ src/components/ProductSearch/Products.tsx
@@ -3,9 +3,10 @@ import { ProductType, ProductsType } from "@/service/types";
 
 type ProductsProps = {
     products: ProductsType;
+    error: Error | undefined;
 };
 
-export const Products = ({ products }: ProductsProps) => {
+export const Products = ({ products, error }: ProductsProps) => {
     const productsList = products?.products.map((v: ProductType) => (
         <ProductGrid.Product
             key={v.id}
@@ -18,6 +19,9 @@ export const Products = ({ products }: ProductsProps) => {
             stock={v.stock}
         />
     ));
+
+    if (error) return <p>Could not load the products</p>;
+    if (!products) return <p>Loading...</p>;
     return (
         <div className="w-11/12 desktop:ml-14 desktop:grid-cols-5 desktop:gap-4 laptop:ml-10 laptop:grid-cols-4 laptop:gap-3 largecellphone:ml-8 largecellphone:grid-cols-3 cellphone:ml-4  cellphone:grid-cols-2 cellphone:gap-2 justify-center mt-10 grid">
             {productsList}
